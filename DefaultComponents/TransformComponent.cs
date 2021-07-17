167ce4e8b57305359e9075725a2e2c99dfbe630e
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-07-17 06:20:02 +0300

    upd: return partial method to transform

--- DefaultComponents/TransformComponent.cs
+++ DefaultComponents/TransformComponent.cs
@@ -2,12 +2,14 @@
 
 namespace Components
 {
-    public partial class TransformComponent
+    public partial class TransformComponent : BaseComponent
     {
         [Field(0)]
         public Vector3Serialize PositionSave;
 
         [Field(1)]
         public Vector3Serialize RotationSave;
+
+        partial void InfoUpdated();
     }
 }
