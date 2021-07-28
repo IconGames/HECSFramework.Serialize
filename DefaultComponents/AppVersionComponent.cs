58a3e816cc37bcdf6935f3cec2bf3c53d7eb121d
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-07-28 18:22:45 +0300

    add: app version serialization

---
+++ DefaultComponents/AppVersionComponent.cs
@@ -0,0 +1,20 @@
+﻿using HECSFramework.Core;
+
+namespace Components
+{
+    public partial class AppVersionComponent : IBeforeSerializationComponent, IAfterSerializationComponent
+    {
+        [Field(0)]
+        public int VersionSerialize { get; set; }
+        
+        public void AfterSync()
+        {
+            Version = VersionSerialize;
+        }
+
+        public void BeforeSync()
+        {
+            VersionSerialize = Version;
+        }
+    }
+}
