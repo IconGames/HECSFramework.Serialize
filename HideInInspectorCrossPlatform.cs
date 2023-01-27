febacdc4ebbaa8a97d5490d89d48eceb19d488e6
evgenii.dubovik@playstrom.io <evgenii.dubovik@playstrom.io>
2023-01-27 14:35:24 +0300

    mov: hide cross

--- HideInInspectorCrossPlatform.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace HECSFramework.Core
-{
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
-    public class HideInInspectorCrossPlatform : Attribute
-    {
-    }
-}
